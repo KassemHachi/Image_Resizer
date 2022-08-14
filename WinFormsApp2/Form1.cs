818103a6eba0d41ba7b125b3f13e29487e3cf18c
KassemHachi <kassemhachi@gmail.com>
2022-08-14 18:40:30 +0100

    Add list of sizes

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -9,17 +9,75 @@ namespace WinFormsApp2
         {
             InitializeComponent();
         }
-
+        private void ResizeFunc(int width,int Height, string path)
+        {
+            using var image = SixLabors.ImageSharp.Image.Load(path);
+            image.Mutate(x => x.Resize( width,Height));
+            image.Save(Path.GetDirectoryName(path) +"\\output_"+width.ToString()+"_"+ ".png");
+        }
+        string pathImage = "";
+        string imageName = "";
         private void button1_Click(object sender, EventArgs e)
         {
+            
             if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
             {
-               
-
-                using var image = SixLabors.ImageSharp.Image.Load(openFileDialog1.FileName);
-                image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
-                image.Save(Path.GetDirectoryName(openFileDialog1.FileName)+"\\output.png");
+                pathImage = openFileDialog1.FileName;
+                
+                label1.Text = pathImage;
+                
             }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                for (int x = 0; x < listBox1.Items.Count; x++)
+                {
+                    ResizeFunc(int.Parse(listBox1.Items[x].ToString()), int.Parse(listBox2.Items[x].ToString()), pathImage); ;
+
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value !=0 && numericUpDown2.Value != 0)
+            {
+                listBox1.Items.Add(numericUpDown1.Value);
+                listBox2.Items.Add(numericUpDown2.Value);
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex=listBox2.SelectedIndex;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.SelectedIndex = listBox1.SelectedIndex;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int selectedIndex = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(selectedIndex);
+            listBox2.Items.RemoveAt(selectedIndex);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
